f7d613abaf625e0b05ac415103e1a94dea20af0e
Jan Joneš <jjones@outlook.cz>
2017-02-25 17:55:43 +0100

    fat mach-o parsing implementation started

--- src/JJones.IPASimulator.WinApp/MainPage.xaml.cs
+++ src/JJones.IPASimulator.WinApp/MainPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MiscUtil.Conversion;
+using MiscUtil.IO;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text.RegularExpressions;
 using Windows.ApplicationModel;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -27,7 +31,37 @@ namespace JJones.IPASimulator.WinApp
             InitializeComponent();
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void loadIPAButton_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".ipa");
+            picker.FileTypeFilter.Add(".zip");
+            picker.SuggestedStartLocation = PickerLocationId.Downloads;
+            picker.ViewMode = PickerViewMode.List;
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                var folder = await ApplicationData.Current.LocalCacheFolder.CreateFolderAsync("IPAs", CreationCollisionOption.OpenIfExists);
+                file = await file.CopyAsync(folder, "Application.ipa", NameCollisionOption.ReplaceExisting);
+
+                using (var archive = ZipFile.OpenRead(file.Path))
+                {
+                    var appRegex = new Regex(@"Payload/([^/]+)\.app/\1", RegexOptions.IgnoreCase);
+                    var appEntry = archive.Entries.FirstOrDefault(z => appRegex.IsMatch(z.FullName));
+                    using (var str = appEntry.Open())
+                    using (var rdr = new EndianBinaryReader(EndianBitConverter.Big, str))
+                    {
+                        var magic = rdr.ReadUInt32();
+                        if (magic == 0xCAFEBABE)
+                        {
+                            var nfat_arch = rdr.ReadUInt32();
+
+                        }
+                    }
+                }
+            }
+        }
+        private async void loadDllButton_Click(object sender, RoutedEventArgs e)
         {
             var picker = new FileOpenPicker();
             picker.FileTypeFilter.Add(".dll");
@@ -36,7 +70,8 @@ namespace JJones.IPASimulator.WinApp
             var file = await picker.PickSingleFileAsync();
             if (file != null)
             {
-                var target = await file.CopyAsync(Package.Current.InstalledLocation, file.Name, NameCollisionOption.ReplaceExisting);
+                var folder = await ApplicationData.Current.LocalCacheFolder.CreateFolderAsync("DLLs", CreationCollisionOption.OpenIfExists);
+                file = await file.CopyAsync(folder, "Application.dll", NameCollisionOption.ReplaceExisting);
             }
         }
     }
