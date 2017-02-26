22635c0e36c3b0bf54738f747544d8fd88a190e0
Jan Joneš <jjones@outlook.cz>
2017-02-26 08:27:43 +0100

    unicorn loaded in console app

--- src/JJones.IPASimulator.ConApp/Program.cs
+++ src/JJones.IPASimulator.ConApp/Program.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UnicornManaged;
+using UnicornManaged.Const;
 
 namespace JJones.IPASimulator.ConApp
 {
@@ -12,9 +14,13 @@ namespace JJones.IPASimulator.ConApp
     {
         static void Main(string[] args)
         {
-            var file = PeLib.PeLib_openFile("output.exe");
-            var mzHeader = PeLib.PeFile_mzHeader(file);
-            var peHeader = PeLib.PeFile_peHeader(file);
+            //var file = PeLib.PeLib_openFile("output.exe");
+            //var mzHeader = PeLib.PeFile_mzHeader(file);
+            //var peHeader = PeLib.PeFile_peHeader(file);
+
+            var uni = new Unicorn(Common.UC_ARCH_ARM, Common.UC_MODE_ARM);
+            Console.WriteLine(uni.Version());
+            Console.ReadLine();
         }
     }
 }
