e8894917cfa935bdbd1636e903b331ccad670de7
Jan Joneš <jjones@outlook.cz>
2017-12-08 01:15:46 +0100

    uwp app project added

---
+++ src/IpaSimulator/MainPage.xaml.cpp
@@ -0,0 +1,32 @@
+//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+#include <LIEF/LIEF.hpp>
+
+using namespace IpaSimulator;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace std;
+using namespace LIEF::MachO;
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+    FatBinary* bin = Parser::parse("test.ipa");
+    cout << bin << endl;
+    delete bin;
+}
