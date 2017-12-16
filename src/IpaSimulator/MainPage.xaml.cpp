12463b5df557a4dfe7a1b06aad2c4d17d1f5f67e
Jan Joneš <jjones@outlook.cz>
2017-12-16 12:52:41 +0100

    arm unicorn example added

--- src/IpaSimulator/MainPage.xaml.cpp
+++ src/IpaSimulator/MainPage.xaml.cpp
@@ -9,6 +9,7 @@
 #include <LIEF/filesystem/filesystem.h>
 #include <sstream>
 #include <unicorn/unicorn.h>
+#include <string.h>
 
 using namespace IpaSimulator;
 using namespace Platform;
@@ -25,66 +26,184 @@ using namespace Windows::UI::Xaml::Navigation;
 using namespace std;
 using namespace LIEF::MachO;
 
+/* Sample code to demonstrate how to emulate ARM code */
+
+// code to be emulated
+#define ARM_CODE "\x37\x00\xa0\xe3\x03\x10\x42\xe0" // mov r0, #0x37; sub r1, r2, r3
+#define THUMB_CODE "\x83\xb0" // sub    sp, #0xc
+
+// memory address where emulation starts
+#define ADDRESS 0x10000
+
+static void hook_block(uc_engine *uc, uint64_t address, uint32_t size, void *user_data)
+{
+    printf(">>> Tracing basic block at 0x%llx, block size = 0x%x\n", address, size);
+}
+
+static void hook_code(uc_engine *uc, uint64_t address, uint32_t size, void *user_data)
+{
+    printf(">>> Tracing instruction at 0x%llx, instruction size = 0x%x\n", address, size);
+}
+
+static void test_arm(void)
+{
+    uc_engine *uc;
+    uc_err err;
+    uc_hook trace1, trace2;
+
+    int r0 = 0x1234;     // R0 register
+    int r2 = 0x6789;     // R1 register
+    int r3 = 0x3333;     // R2 register
+    int r1;     // R1 register
+
+    printf("Emulate ARM code\n");
+
+    // Initialize emulator in ARM mode
+    err = uc_open(UC_ARCH_ARM, UC_MODE_ARM, &uc);
+    if (err) {
+        printf("Failed on uc_open() with error returned: %u (%s)\n",
+                err, uc_strerror(err));
+        return;
+    }
+
+    // map 2MB memory for this emulation
+    uc_mem_map(uc, ADDRESS, 2 * 1024 * 1024, UC_PROT_ALL);
+
+    // write machine code to be emulated to memory
+    uc_mem_write(uc, ADDRESS, ARM_CODE, sizeof(ARM_CODE) - 1);
+
+    // initialize machine registers
+    uc_reg_write(uc, UC_ARM_REG_R0, &r0);
+    uc_reg_write(uc, UC_ARM_REG_R2, &r2);
+    uc_reg_write(uc, UC_ARM_REG_R3, &r3);
+
+    // tracing all basic blocks with customized callback
+    uc_hook_add(uc, &trace1, UC_HOOK_BLOCK, hook_block, NULL, 1, 0);
+
+    // tracing one instruction at ADDRESS with customized callback
+    uc_hook_add(uc, &trace2, UC_HOOK_CODE, hook_code, NULL, ADDRESS, ADDRESS);
+
+    // emulate machine code in infinite time (last param = 0), or when
+    // finishing all the code.
+    err = uc_emu_start(uc, ADDRESS, ADDRESS + sizeof(ARM_CODE) -1, 0, 0);
+    if (err) {
+        printf("Failed on uc_emu_start() with error returned: %u\n", err);
+    }
+
+    // now print out some registers
+    printf(">>> Emulation done. Below is the CPU context\n");
+
+    uc_reg_read(uc, UC_ARM_REG_R0, &r0);
+    uc_reg_read(uc, UC_ARM_REG_R1, &r1);
+    printf(">>> R0 = 0x%x\n", r0);
+    printf(">>> R1 = 0x%x\n", r1);
+
+    uc_close(uc);
+}
+
+static void test_thumb(void)
+{
+    uc_engine *uc;
+    uc_err err;
+    uc_hook trace1, trace2;
+
+    int sp = 0x1234;     // R0 register
+
+    printf("Emulate THUMB code\n");
+
+    // Initialize emulator in ARM mode
+    err = uc_open(UC_ARCH_ARM, UC_MODE_THUMB, &uc);
+    if (err) {
+        printf("Failed on uc_open() with error returned: %u (%s)\n",
+                err, uc_strerror(err));
+        return;
+    }
+
+    // map 2MB memory for this emulation
+    uc_mem_map(uc, ADDRESS, 2 * 1024 * 1024, UC_PROT_ALL);
+
+    // write machine code to be emulated to memory
+    uc_mem_write(uc, ADDRESS, THUMB_CODE, sizeof(THUMB_CODE) - 1);
+
+    // initialize machine registers
+    uc_reg_write(uc, UC_ARM_REG_SP, &sp);
+
+    // tracing all basic blocks with customized callback
+    uc_hook_add(uc, &trace1, UC_HOOK_BLOCK, hook_block, NULL, 1, 0);
+
+    // tracing one instruction at ADDRESS with customized callback
+    uc_hook_add(uc, &trace2, UC_HOOK_CODE, hook_code, NULL, ADDRESS, ADDRESS);
+
+    // emulate machine code in infinite time (last param = 0), or when
+    // finishing all the code.
+    // Note we start at ADDRESS | 1 to indicate THUMB mode.
+    err = uc_emu_start(uc, ADDRESS | 1, ADDRESS + sizeof(THUMB_CODE) -1, 0, 0);
+    if (err) {
+        printf("Failed on uc_emu_start() with error returned: %u\n", err);
+    }
+
+    // now print out some registers
+    printf(">>> Emulation done. Below is the CPU context\n");
+
+    uc_reg_read(uc, UC_ARM_REG_SP, &sp);
+    printf(">>> SP = 0x%x\n", sp);
+
+    uc_close(uc);
+}
+
 MainPage::MainPage()
 {
 	InitializeComponent();
 
+#if 0
     filesystem::path dir(ApplicationData::Current->TemporaryFolder->Path->Data());
     filesystem::path file("todo.ipa");
     filesystem::path full = dir / file;
     FatBinary* fat = Parser::parse(full.str()); // TODO: maybe convert to unique_ptr
     Binary& bin = fat->at(0);
-#if 0
     for (auto& c : bin.commands()) {
         uint32_t s = c.size();
         buffer_t b = c.data();
     }
-#endif
     delete fat;
+#endif
 
-#define X86_CODE32 "\x41\x4a"
-#define ADDRESS 0x1000000
-    uc_engine *uc;
-    uc_err err;
-    int r_ecx = 0x1234;     // ECX register
-    int r_edx = 0x7890;     // EDX register
-    
-    printf("Emulate i386 code\n");
-    
-    // Initialize emulator in X86-32bit mode
-    err = uc_open(UC_ARCH_X86, UC_MODE_32, &uc);
-    if (err != UC_ERR_OK) {
-      printf("Failed on uc_open() with error returned: %u\n", err);
-      return;
+    //Redirect stdout to a file
+    Platform::String^ outfilename = Windows::Storage::ApplicationData::Current->LocalFolder->Path + L"\\stdout.txt";
+    FILE *stdout_capture;
+    if ((stdout_capture = _wfreopen(outfilename->Data(), L"w", stdout)) == NULL)
+    {
+        OutputDebugString(L"Couldn't open file to redirect stdout\n");
+        exit(-1);
     }
-    
-    // map 2MB memory for this emulation
-    uc_mem_map(uc, ADDRESS, 2 * 1024 * 1024, UC_PROT_ALL);
-    
-    // write machine code to be emulated to memory
-    if (uc_mem_write(uc, ADDRESS, X86_CODE32, sizeof(X86_CODE32) - 1)) {
-      printf("Failed to write emulation code to memory, quit!\n");
-      return;
+
+    //Print file location to OutputDebugString
+    OutputDebugString(L"Redirecting stdout to ");
+    OutputDebugString(outfilename->Data());
+    OutputDebugString(L"\n");
+
+    test_arm();
+    printf("==========================\n");
+    test_thumb();
+
+    //End capturing stdout
+    freopen("CON", "w", stdout);
+    fclose(stdout_capture);
+
+    //Output the captured stdout to OutputDebugString
+    if ((stdout_capture = _wfreopen(outfilename->Data(), L"r", stdout)) == NULL)
+    {
+        OutputDebugString(L"Couldn't open file to read redirected stdout\n");
+        exit(-1);
     }
-    
-    // initialize machine registers
-    uc_reg_write(uc, UC_X86_REG_ECX, &r_ecx);
-    uc_reg_write(uc, UC_X86_REG_EDX, &r_edx);
-    
-    // emulate code in infinite time & unlimited instructions
-    err=uc_emu_start(uc, ADDRESS, ADDRESS + sizeof(X86_CODE32) - 1, 0, 0);
-    if (err) {
-      printf("Failed on uc_emu_start() with error returned %u: %s\n",
-        err, uc_strerror(err));
-    }
-    
-    // now print out some registers
-    printf("Emulation done. Below is the CPU context\n");
-    
-    uc_reg_read(uc, UC_X86_REG_ECX, &r_ecx);
-    uc_reg_read(uc, UC_X86_REG_EDX, &r_edx);
-    printf(">>> ECX = 0x%x\n", r_ecx);
-    printf(">>> EDX = 0x%x\n", r_edx);
-    
-    uc_close(uc);
+    char* buffer;
+    fseek(stdout_capture, 0, SEEK_END);
+    size_t stdout_len = ftell(stdout_capture);
+    rewind(stdout_capture);
+    buffer = (char*)malloc((sizeof(char)*stdout_len));
+    memset(buffer, 0, (sizeof(char)*stdout_len));
+    size_t result = fread(buffer, 1, stdout_len, stdout_capture);
+    OutputDebugStringA(buffer);
+    free(buffer);
+    fclose(stdout_capture);
 }
