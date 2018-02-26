8fa56cc85d67d5ae0399d7562fbfa1643382b754
Jan Joneš <jjones@outlook.cz>
2018-02-26 14:34:28 +0100

    more options specified (backup)

--- src/HeadersAnalyzer/HeadersAnalyzer.cpp
+++ src/HeadersAnalyzer/HeadersAnalyzer.cpp
@@ -1,6 +1,7 @@
 // HeadersAnalyzer.cpp : Defines the entry point for the console application.
 //
 
+#include <iostream>
 #include <clang/Frontend/CompilerInstance.h>
 #include <clang/Basic/TargetOptions.h>
 #include <clang/Basic/TargetInfo.h>
@@ -20,20 +21,23 @@ int main()
     CompilerInstance ci;
     ci.createDiagnostics();
 
-    //ci.getLangOpts().ObjC2 = 1;
-
     //ci.getHeaderSearchOpts().Sysroot = "C:/Users/Jones/Files/Projects/IPASimulator/deps/headers/iPhoneOS11.1.sdk/";
     ci.getHeaderSearchOpts().AddPath("C:/Users/Jones/Files/Projects/IPASimulator/deps/headers/iPhoneOS11.1.sdk/System/Library/Frameworks/", IncludeDirGroup::Angled, /*IsFramework*/ true, /*IgnoreSysRoot*/ false);
     ci.getHeaderSearchOpts().AddPath("C:/Users/Jones/Files/Projects/IPASimulator/deps/headers/iPhoneOS11.1.sdk/usr/include/", IncludeDirGroup::Angled, /*IsFramework*/ false, /*IgnoreSysRoot*/ false);
+    ci.getHeaderSearchOpts().AddPath("C:/Users/Jones/Files/Projects/IPASimulator/deps/clang/lib/Headers/", IncludeDirGroup::Angled, /*IsFramework*/ false, /*IgnoreSysRoot*/ false);
+    //ci.getHeaderSearchOpts().ResourceDir = "C:/Users/Jones/Files/Projects/IPASimulator/deps/clang/lib/Headers/";
 
     auto targetOpts = make_shared<TargetOptions>();
-    targetOpts->Triple = llvm::sys::getDefaultTargetTriple();
+    targetOpts->Triple = "armv7-apple-darwin"; // TODO: just a wild guess
     ci.setTarget(TargetInfo::CreateTargetInfo(ci.getDiagnostics(), targetOpts)); // TODO: TargetInfo* should be deleted when not needed anymore
 
     ci.createFileManager();
     ci.createSourceManager(ci.getFileManager());
-    ci.createPreprocessor(TranslationUnitKind::TU_Complete);
+
     //ci.getPreprocessorOpts().UsePredefines = false;
+    ci.createPreprocessor(TranslationUnitKind::TU_Complete);
+
+    CompilerInvocation::setLangDefaults(ci.getLangOpts(), InputKind::ObjC, ci.getTarget().getTriple(), ci.getPreprocessorOpts());
 
     ci.setASTConsumer(make_unique<ASTConsumer>());
     ci.createASTContext();
