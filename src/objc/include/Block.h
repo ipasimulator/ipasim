daa117715946ea620a4f6263b9b14502942bbd3c
Jan Joneš <jjones@outlook.cz>
2018-07-05 18:00:22 +0200

    Added "Block.h".

---
+++ src/objc/include/Block.h
@@ -0,0 +1,4 @@
+// This is a header of BlocksRuntime. See http://compiler-rt.llvm.org/ for clang's implementation.
+// Other implementations: http://mackyle.github.io/blocksruntime/, https://github.com/gnustep/libobjc2.
+// TODO: Use the correct header (corresponding to the implementation used).
+#include "MacOSX10.13.sdk/usr/include/Block.h"
