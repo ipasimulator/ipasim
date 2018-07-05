0d48bd405e81a9094f4f308fadba0d096241eec4
Jan Joneš <jjones@outlook.cz>
2018-07-05 12:23:44 +0200

    [objc] Started building "build.cmd".

--- src/objc/include/TargetConditionals.h
+++ src/objc/include/TargetConditionals.h
@@ -1,1 +1,12 @@
+#ifndef OBJC_PORT
 #include "MacOSX10.13.sdk/usr/include/TargetConditionals.h"
+#else
+#define TARGET_CPU_X86 1
+
+#define TARGET_OS_MAC 1
+#define TARGET_OS_IPHONE 1
+#define TARGET_OS_IOS 1
+
+#define TARGET_RT_LITTLE_ENDIAN 1
+#define TARGET_RT_MAC_MACHO 1
+#endif
