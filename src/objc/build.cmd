86982a43f0bcb6d30bee8436862f304af1c7639c
Jan Joneš <jjones@outlook.cz>
2018-07-09 19:55:09 +0200

    [objc] Accepted additional arguments in "build.cmd".

--- src/objc/build.cmd
+++ src/objc/build.cmd
@@ -1,14 +1,14 @@
 @echo off
-call build_one.cmd hashtable2.mm
-call build_one.cmd maptable.mm
-call build_one.cmd NSObject.mm
-call build_one.cmd objc-accessors.mm
-call build_one.cmd objc-auto.mm
-call build_one.cmd objc-block-trampolines.mm
-call build_one.cmd objc-cache-old.mm
-call build_one.cmd objc-cache.mm
-call build_one.cmd objc-class-old.mm
-call build_one.cmd objc-class.mm
-call build_one.cmd objc-errors.mm
-call build_one.cmd objc-exception.mm
-call build_one.cmd objc-file-old.mm
+call build_one.cmd hashtable2.mm %*
+call build_one.cmd maptable.mm %*
+call build_one.cmd NSObject.mm %*
+call build_one.cmd objc-accessors.mm %*
+call build_one.cmd objc-auto.mm %*
+call build_one.cmd objc-block-trampolines.mm %*
+call build_one.cmd objc-cache-old.mm %*
+call build_one.cmd objc-cache.mm %*
+call build_one.cmd objc-class-old.mm %*
+call build_one.cmd objc-class.mm %*
+call build_one.cmd objc-errors.mm %*
+call build_one.cmd objc-exception.mm %*
+call build_one.cmd objc-file-old.mm %*
