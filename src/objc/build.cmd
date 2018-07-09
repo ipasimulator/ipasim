284b1eab61c4529c4c6808f0bf6af9a51688cc15
Jan Joneš <jjones@outlook.cz>
2018-07-09 15:22:36 +0200

    [objc] Ported "objc-cache.mm".

--- src/objc/build.cmd
+++ src/objc/build.cmd
@@ -5,3 +5,5 @@ call build_one.cmd NSObject.mm
 call build_one.cmd objc-accessors.mm
 call build_one.cmd objc-auto.mm
 call build_one.cmd objc-block-trampolines.mm
+call build_one.cmd objc-cache-old.mm
+call build_one.cmd objc-cache.mm
