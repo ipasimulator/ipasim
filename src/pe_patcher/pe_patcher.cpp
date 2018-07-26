2fb5204992b9bede8d115fb2daeae83777734829
Jan Joneš <jjones@outlook.cz>
2018-07-26 12:31:21 +0200

    [pep] Added simple `.mhdr` section (backup).

--- src/pe_patcher/pe_patcher.cpp
+++ src/pe_patcher/pe_patcher.cpp
@@ -12,7 +12,31 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    std::unique_ptr<LIEF::Binary> binary = LIEF::Parser::parse(argv[1]);
-    std::cout << *binary << std::endl;
+    std::unique_ptr<LIEF::PE::Binary> binary = LIEF::PE::Parser::parse(argv[1]);
+
+    // Create `.mhdr` section.
+    LIEF::PE::Section mhdrSection;
+    mhdrSection.name(".mhdr");
+    const char *data = "Hello, .mhdr section!";
+    mhdrSection.content(vector<uint8_t>(data, data + strlen(data)));
+    mhdrSection = binary->add_section(mhdrSection, LIEF::PE::PE_SECTION_TYPES::DATA);
+
+    // Move the `.mhdr` section to the top.
+    // TODO: It doesn't seem to be working.
+    auto start = binary->sections();
+    auto it = --binary->sections().end();
+    assert(*it == mhdrSection);
+    for (; it != start;) {
+        auto curr = it--;
+        swap(*it, *curr);
+    }
+
+    // TODO: Rebuild the binary (maybe also rearrange the sections to group them into segments).
+    //LIEF::PE::Builder builder(binary.get());
+    //builder.build_imports(true);
+    //builder.patch_imports(true);
+    binary->write(string(argv[1]) + "-edit.exe");
+
+    cout << "Done" << endl;
     return 0;
 }
