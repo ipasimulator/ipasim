40e68aa0e5a8336da43bb4c88a5103bffec4ab23
Jan Joneš <jjones@outlook.cz>
2018-08-12 14:46:34 +0200

    [pep] Updated `.mhdr` section characteristics.

--- src/pe_patcher/pe_patcher.cpp
+++ src/pe_patcher/pe_patcher.cpp
@@ -17,12 +17,40 @@ int main(int argc, char** argv)
 
     std::unique_ptr<LIEF::PE::Binary> binary = LIEF::PE::Parser::parse(argv[1]);
 
+    // Find the lowest virtual address used.
+    uint64_t va = numeric_limits<uint64_t>::max();
+    for (auto &&section : binary->sections()) {
+        uint64_t sva = section.virtual_address();
+        if (sva < va) { va = sva; }
+    }
+
     // Create `.mhdr` section.
     LIEF::PE::Section mhdrSection;
     mhdrSection.name(".mhdr");
+
+    // Set its content.
     const char *data = "Hello, .mhdr section!";
-    mhdrSection.content(vector<uint8_t>(data, data + strlen(data)));
-    mhdrSection = binary->add_section(mhdrSection, LIEF::PE::PE_SECTION_TYPES::DATA);
+    uint64_t size = strlen(data);
+    mhdrSection.content(vector<uint8_t>(data, data + size));
+
+    // Set its RVA.
+    // TODO: Changing virtual_address makes the resulting PE binary invalid. Why?
+    //mhdrSection.virtual_address(va - size); // TODO: Check that it's non-negative.
+    //mhdrSection.virtual_address(0x34000 << 2);
+
+    // Set its characteristics.
+    mhdrSection.characteristics_list().clear();
+    mhdrSection.add_characteristic(LIEF::PE::SECTION_CHARACTERISTICS::IMAGE_SCN_CNT_INITIALIZED_DATA);
+    mhdrSection.add_characteristic(LIEF::PE::SECTION_CHARACTERISTICS::IMAGE_SCN_MEM_READ);
+
+    mhdrSection = binary->add_section(mhdrSection);
+
+    // Remove some flags. TODO: Probably not needed.
+    binary->optional_header().dll_characteristics_list().erase(LIEF::PE::DLL_CHARACTERISTICS::IMAGE_DLL_CHARACTERISTICS_DYNAMIC_BASE);
+    binary->optional_header().dll_characteristics_list().erase(LIEF::PE::DLL_CHARACTERISTICS::IMAGE_DLL_CHARACTERISTICS_NX_COMPAT);
+
+    // Save the binary.
+    binary->write(string(argv[1]) + "-edit.exe");
 
     // Move the `.mhdr` section to the top.
     // TODO: It doesn't seem to be working.
@@ -38,13 +66,13 @@ int main(int argc, char** argv)
     //LIEF::PE::Builder builder(binary.get());
     //builder.build_imports(true);
     //builder.patch_imports(true);
-    //binary->write(string(argv[1]) + "-edit.exe");
+    //
 
-    LIEF::PE::Builder builder(binary.get());
+    /*LIEF::PE::Builder builder(binary.get());
     builder.build();
     builder.write(string(argv[1]) + "-edit.exe");
 
-    LIEF::PE::Binary patched(binary->name(), binary->type());
+    LIEF::PE::Binary patched(binary->name(), binary->type());*/
 
     cout << "Done" << endl;
     return 0;
