f8deea0118b4352950213694fcc6922a709e40ef
Jan Joneš <jjones@outlook.cz>
2018-08-13 23:24:55 +0200

    Added sample implementation of `_dyld_initialize`.

--- src/dyld/dyld.h
+++ src/dyld/dyld.h
@@ -1,18 +1,20 @@
 #ifndef DYLD_H
 #define DYLD_H
 
+#include <llvm/BinaryFormat/MachO.h>
+
 #define EXTERN extern "C"
 #if defined(BUILDING_DYLD)
-#define API __declspec(dllexport) EXTERN
+#define API EXTERN __declspec(dllexport)
 #else
-#define API __declspec(dllimport) EXTERN
+#define API EXTERN __declspec(dllimport)
 #endif
 
-typedef void(*_dyld_objc_notify_mapped)(unsigned count, const char* const paths[], const struct mach_header* const mh[]);
-typedef void(*_dyld_objc_notify_init)(const char* path, const struct mach_header* mh);
-typedef void(*_dyld_objc_notify_unmapped)(const char* path, const struct mach_header* mh);
+typedef void(*_dyld_objc_notify_mapped)(unsigned count, const char* const paths[], const llvm::MachO::mach_header* const mh[]);
+typedef void(*_dyld_objc_notify_init)(const char* path, const llvm::MachO::mach_header* mh);
+typedef void(*_dyld_objc_notify_unmapped)(const char* path, const llvm::MachO::mach_header* mh);
 
-API void _dyld_initialize(const struct mach_header* mh);
+API void _dyld_initialize(const llvm::MachO::mach_header* mh);
 API void _dyld_objc_notify_register(_dyld_objc_notify_mapped mapped,
     _dyld_objc_notify_init init,
     _dyld_objc_notify_unmapped unmapped);
--- src/dyld/stdafx.h
+++ src/dyld/stdafx.h
@@ -8,7 +8,9 @@
 #include "targetver.h"
 
 #define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
+#define NOMINMAX
 // Windows Header Files:
 #include <windows.h>
 
 #include "dyld.h"
+#include <iostream>
