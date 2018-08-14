0d3dc10cb0e39cb1669a663441bd37d287400736
Jan Joneš <jjones@outlook.cz>
2018-08-14 09:28:56 +0200

    [dyld] Added class `library_guard`.

--- src/dyld/dyld.cpp
+++ src/dyld/dyld.cpp
@@ -7,6 +7,20 @@ using namespace llvm;
 using namespace MachO;
 using namespace std;
 
+class library_guard {
+public:
+    library_guard(const char *name) : handle_(LoadLibraryA(name)) {}
+    ~library_guard() {
+        if (handle_) {
+            FreeLibrary(handle_);
+        }
+    }
+    operator bool() { return handle_; }
+    FARPROC get_symbol(LPCSTR name) { return GetProcAddress(handle_, name); }
+private:
+    HMODULE handle_;
+};
+
 struct dylib_info {
     const char *path;
     const mach_header *header;
@@ -25,8 +39,8 @@ void _dyld_initialize(const mach_header* mh) {
             const char *name = reinterpret_cast<const char *>(dylib) + dylib->dylib.name;
 
             // Try to get its Mach-O header.
-            if (HMODULE lib = LoadLibraryA(name)) {
-                if (auto sym = reinterpret_cast<const mach_header *>(GetProcAddress(lib, "_mh_dylib_header"))) {
+            if (auto lib = library_guard(name)) {
+                if (auto sym = reinterpret_cast<const mach_header *>(lib.get_symbol("_mh_dylib_header"))) {
 
                     // If successfull, save it.
                     dylibs.push_back(dylib_info{ name, sym });
