38d7f931bfad27e5d5f14950f019f74c09cb1d52
Jan Joneš <jjones@outlook.cz>
2018-08-17 23:10:28 +0200

    [sam] Updated include directories.

--- samples/export-class/main.mm
+++ samples/export-class/main.mm
@@ -1,9 +1,9 @@
 // Compiled with:
 // TODO: Doesn't work due to the `-fuse-ld` option for some reason.
-// ..\..\build\bin\clang -target i386-pc-windows-msvc main.mm -o build/main.exe -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\build\bin\lld-link -L "..\..\src\objc\Debug" -L "..\..\Debug" -L ".\build" -llibobjc.A -ltestclass -ldyld -Wl,-debug -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
+// ..\..\build\bin\clang -target i386-pc-windows-msvc main.mm -o build/main.exe -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\build\bin\lld-link -L "..\..\src\objc\Debug" -L "..\..\Debug" -L ".\build" -llibobjc.A -ltestclass -ldyld -Wl,-debug -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\Debug\include"
 
 // To get .obj:
-// ..\..\build\bin\clang -target i386-pc-windows-msvc -c main.mm -o build/main-obj.obj -fobjc-runtime=ios-11 -g -gcodeview -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
+// ..\..\build\bin\clang -target i386-pc-windows-msvc -c main.mm -o build/main-obj.obj -fobjc-runtime=ios-11 -g -gcodeview -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\Debug\include"
 
 // To link the .obj with testclass.dll:
 // ..\..\build\bin\lld-link -out:build\main.exe -debug -libpath:"..\..\src\objc\Debug" -libpath:"..\..\Debug" -libpath:".\build" main-obj.obj testclass.lib libcmt.lib libobjc.A.lib dyld.lib
