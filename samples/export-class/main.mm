d6bbb28d22c50ec910da208de40a1b656992a7d3
Jan Joneš <jjones@outlook.cz>
2018-08-17 11:52:32 +0200

    [sam] Updated build instructions.

--- samples/export-class/main.mm
+++ samples/export-class/main.mm
@@ -1,12 +1,12 @@
 // Compiled with:
 // TODO: Doesn't work due to the `-fuse-ld` option for some reason.
-// ..\..\deps\llvm\build\Release\bin\clang -target i386-pc-windows-msvc main.mm -o build/main.exe -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\deps\llvm\build\Release\bin\lld-link -L "..\..\src\objc\Debug" -L "..\..\Debug" -L ".\build" -llibobjc.A -ltestclass -ldyld -Wl,-debug -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
+// ..\..\build\bin\clang -target i386-pc-windows-msvc main.mm -o build/main.exe -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\build\bin\lld-link -L "..\..\src\objc\Debug" -L "..\..\Debug" -L ".\build" -llibobjc.A -ltestclass -ldyld -Wl,-debug -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
 
 // To get .obj:
-// ..\..\deps\llvm\build\Release\bin\clang -target i386-pc-windows-msvc -c main.mm -o build/main-obj.obj -fobjc-runtime=ios-11 -g -gcodeview -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
+// ..\..\build\bin\clang -target i386-pc-windows-msvc -c main.mm -o build/main-obj.obj -fobjc-runtime=ios-11 -g -gcodeview -I "..\..\deps\llvm\include" -I "..\..\deps\llvm\build\include"
 
 // To link the .obj with testclass.dll:
-// ..\..\deps\llvm\build\Release\bin\lld-link -out:build\main.exe -debug -libpath:"..\..\src\objc\Debug" -libpath:"..\..\Debug" -libpath:".\build" main-obj.obj testclass.lib libcmt.lib libobjc.A.lib dyld.lib
+// ..\..\build\bin\lld-link -out:build\main.exe -debug -libpath:"..\..\src\objc\Debug" -libpath:"..\..\Debug" -libpath:".\build" main-obj.obj testclass.lib libcmt.lib libobjc.A.lib dyld.lib
 
 #include "..\..\src\dyld\dyld.h"
 #include "testclass.h"
--- samples/export-class/testclass.mm
+++ samples/export-class/testclass.mm
@@ -1,12 +1,12 @@
 // Compiled with:
 // TODO: Doesn't work due to the `-fuse-ld` option for some reason.
-// ..\..\deps\llvm\build\Release\bin\clang -shared -target i386-pc-windows-msvc testclass.mm -o build/testclass.dll -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\deps\llvm\build\Release\bin\lld-link -L "..\..\src\objc\Debug" -llibobjc.A -Wl,-debug
+// ..\..\build\bin\clang -shared -target i386-pc-windows-msvc testclass.mm -o build/testclass.dll -fobjc-runtime=ios-11 -g -gcodeview -fuse-ld=..\..\build\bin\lld-link -L "..\..\src\objc\Debug" -llibobjc.A -Wl,-debug
 
 // To get .obj:
-// ..\..\deps\llvm\build\Release\bin\clang -target i386-pc-windows-msvc -c testclass.mm -o build/testclass-obj.obj -fobjc-runtime=ios-11 -g -gcodeview
+// ..\..\build\bin\clang -target i386-pc-windows-msvc -c testclass.mm -o build/testclass-obj.obj -fobjc-runtime=ios-11 -g -gcodeview
 
 // To get .dll from the .obj:
-// ..\..\deps\llvm\build\Release\bin\lld-link -out:build\testclass.dll -defaultlib:libcmt -libpath:"..\..\src\objc\Debug" -libpath:".\build" -nologo -debug -dll testclass-obj.obj libobjc.A.lib
+// ..\..\build\bin\lld-link -out:build\testclass.dll -defaultlib:libcmt -libpath:"..\..\src\objc\Debug" -libpath:".\build" -nologo -debug -dll testclass-obj.obj libobjc.A.lib
 
 #include <cstdio> // for puts
 #define BUILDING_TEST
