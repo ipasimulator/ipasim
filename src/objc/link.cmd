71b6a9c69c2ba2ce9b3d18c77f89d47eec362622
Jan Joneš <jjones@outlook.cz>
2018-08-20 13:04:35 +0200

    [objc] Linked with `dyld_initializer.obj`.

--- src/objc/link.cmd
+++ src/objc/link.cmd
@@ -4,4 +4,4 @@ echo.
 echo build.cmd: Linking...
 echo.
 
-"..\..\build\bin\lld-link.exe" -nologo -dll -force:multiple -ignore:4042 -ignore:4049,4217 -def:".\libobjc.def" -out:".\Debug\libobjc.A.dll" -debug "libcmt.lib" "..\..\Debug\pthread\pthread.lib" "..\..\Debug\dyld.lib" @".\Debug\files.txt" %*
+"..\..\build\bin\lld-link.exe" -nologo -dll -force:multiple -ignore:4042 -ignore:4049,4217 -def:".\libobjc.def" -out:".\Debug\libobjc.A.dll" -debug "libcmt.lib" "..\..\Debug\pthread\pthread.lib" "..\..\Debug\dyld.lib" @".\Debug\files.txt" "..\..\Debug\dyld_initializer.obj" %*
