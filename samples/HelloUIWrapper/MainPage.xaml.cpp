c6bac8c089c1f3e72418db9aabb61c72553ea8a6
Jan Joneš <jjones@outlook.cz>
2018-08-28 08:30:12 +0200

    [sam] [HelloUIWrapper] Called `main`.

--- samples/HelloUIWrapper/MainPage.xaml.cpp
+++ samples/HelloUIWrapper/MainPage.xaml.cpp
@@ -23,17 +23,29 @@ using namespace std;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
+template<typename T>
+static T win(T result) {
+    if (!result) {
+        OutputDebugStringA((string("Error: ") + to_string(GetLastError())).c_str());
+    }
+    return result;
+}
+
 MainPage::MainPage()
 {
 	InitializeComponent();
 
     // Let's try to load `HelloUI.exe`.
-    HMODULE lib = LoadPackagedLibrary(L"HelloUI.exe", 0);
-    if (lib) {
-        OutputDebugStringA("Success!");
-        FreeLibrary(lib);
-    }
-    else {
-        OutputDebugStringA((string("Error: ") + to_string(GetLastError())).c_str());
+    if (HMODULE lib = win(LoadPackagedLibrary(L"HelloUI.exe", 0))) {
+
+        // Find it's method `main`.
+        if (FARPROC func = win(GetProcAddress(lib, "main"))) {
+
+            // And call it.
+            char *name = "HelloUI.exe";
+            ((int(*)(int, char **))func)(1, &name);
+        }
+
+        win(FreeLibrary(lib));
     }
 }
