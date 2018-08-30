8508376aba741d5b947b15b9374a7d754eb2b020
Jan Joneš <jjones@outlook.cz>
2018-08-30 21:07:57 +0200

    [rtoc] Added `link.cmd`.

--- src/RTObjCInterop/build.cmd
+++ src/RTObjCInterop/build.cmd
@@ -3,3 +3,9 @@
 if exist ".\Debug\files.txt" del ".\Debug\files.txt"
 
 call build_one.cmd objective-c++ RTObject.mm %*
+call build_one.cmd objective-c++ RTHelpers.mm %*
+call build_one.cmd objective-c++ ObjCHelpers.mm %*
+call build_one.cmd objective-c++ InteropBase.mm %*
+call build_one.cmd c++ dllmain.cpp %*
+
+call link.cmd
