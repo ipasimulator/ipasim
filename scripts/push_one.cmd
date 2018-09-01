85abae15ec472f582e71ee952846c3c1de60250c
Jan Joneš <jjones@outlook.cz>
2018-09-01 08:19:56 +0200

    [deps] Sped up script `push_one.cmd`.

--- scripts/push_one.cmd
+++ scripts/push_one.cmd
@@ -1,10 +1,28 @@
 @echo off
 
+pushd %1
+
+rem Find out if there is anything to be pushed via (fast) command `git status`.
+git status | find /C "branch is ahead" >NUL
+if %errorlevel% equ 0 (goto push) else (goto dont_push)
+
+:dont_push
+
+echo.
+echo Nothing to push in "%1".
+echo.
+
+goto end
+
+rem If there is something to push, call (slow) command `git push`.
+:push
+
 echo.
 echo Pushing "%1"...
 echo.
 
-pushd %1
 git push
 git pushgtm
+
+:end
 popd
