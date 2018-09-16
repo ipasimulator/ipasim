b47c5f5bfbf34bae3b0c1e535e13227acfc9ba44
Jan Joneš <jjones@outlook.cz>
2018-09-16 10:53:48 +0200

    [cg] Added class `LLDBInitializer`.

--- src/HeadersAnalyzer/LLDBHelper.cpp
+++ src/HeadersAnalyzer/LLDBHelper.cpp
@@ -2,11 +2,8 @@
 
 #include "LLDBHelper.hpp"
 
-#include "ErrorReporting.hpp"
-
 #include <Plugins/SymbolFile/PDB/SymbolFilePDB.h>
 #include <lldb/API/SBDebugger.h>
-#include <lldb/Symbol/ObjectFile.h>
 #include <lldb/Utility/DataBufferHeap.h>
 
 #include <llvm/DebugInfo/PDB/PDBSymbolFunc.h>
@@ -18,53 +15,13 @@ using namespace lldb;
 using namespace lldb_private;
 using namespace llvm::pdb;
 
-LLDBHelper::LLDBHelper() {
-  SBDebugger::Initialize();
-  Debugger = Debugger::CreateInstance();
-}
-LLDBHelper::~LLDBHelper() {
-  Debugger::Destroy(Debugger);
-  SBDebugger::Terminate();
-}
+// We do `SBDebugger` initialization in a class separate from `LLDBHelper`, so
+// that `SBDebugger::Terminate` is called after everything else has been
+// destroyed.
+LLDBInitializer::LLDBInitializer() { SBDebugger::Initialize(); }
+LLDBInitializer::~LLDBInitializer() { SBDebugger::Terminate(); }
 
-namespace {
-class ObjectFileUnimplemented : public ObjectFile {
-public:
-  ObjectFileUnimplemented(const lldb::ModuleSP &Module,
-                          const lldb::DataBufferSP &Buffer)
-      : ObjectFile(Module, nullptr, 0, 0, Buffer, 0) {}
-
-#define unimplemented reportFatalError("function is not implemented")
-  void Dump(lldb_private::Stream *S) override { unimplemented; }
-  uint32_t GetAddressByteSize() const override { unimplemented; }
-  uint32_t GetDependentModules(lldb_private::FileSpecList &file_list) override {
-    unimplemented;
-  }
-  bool IsExecutable() const override { unimplemented; }
-  bool GetArchitecture(lldb_private::ArchSpec &arch) override { unimplemented; }
-  void
-  CreateSections(lldb_private::SectionList &unified_section_list) override {
-    unimplemented;
-  }
-  lldb_private::Symtab *GetSymtab() override { unimplemented; }
-  bool IsStripped() override { unimplemented; }
-  bool GetUUID(lldb_private::UUID *uuid) override { unimplemented; }
-  lldb::ByteOrder GetByteOrder() const override { unimplemented; }
-  bool ParseHeader() override { unimplemented; }
-  Type CalculateType() override { unimplemented; }
-  Strata CalculateStrata() override { unimplemented; }
-  lldb_private::ConstString GetPluginName() override { unimplemented; }
-  uint32_t GetPluginVersion() override { unimplemented; }
-#undef unimplemented
-};
-} // namespace
-
-class ObjectFileDummy : public ObjectFileUnimplemented {
-public:
-  ObjectFileDummy(const lldb::ModuleSP &Module,
-                  const lldb::DataBufferSP &Buffer)
-      : ObjectFileUnimplemented(Module, Buffer) {}
-};
+LLDBHelper::LLDBHelper() { Debugger = Debugger::CreateInstance(); }
 
 void LLDBHelper::load(const char *DLL, const char *PDB) {
   // Load PDB into LLDB. This is a hack, actually, because no simple way of
--- src/HeadersAnalyzer/LLDBHelper.hpp
+++ src/HeadersAnalyzer/LLDBHelper.hpp
@@ -4,24 +4,67 @@
 #define LLDBHELPER_HPP
 
 #include "Common.hpp"
+#include "ErrorReporting.hpp"
 
 #include <Plugins/SymbolFile/PDB/SymbolFilePDB.h>
 #include <lldb/Core/Debugger.h>
+#include <lldb/Symbol/ObjectFile.h>
 
 #include <llvm/DebugInfo/PDB/PDBSymbolExe.h>
 
-class ObjectFileDummy;
+class ObjectFileUnimplemented : public lldb_private::ObjectFile {
+public:
+  ObjectFileUnimplemented(const lldb::ModuleSP &Module,
+                          const lldb::DataBufferSP &Buffer)
+      : ObjectFile(Module, nullptr, 0, 0, Buffer, 0) {}
+
+#define unimplemented reportFatalError("function is not implemented")
+  void Dump(lldb_private::Stream *S) override { unimplemented; }
+  uint32_t GetAddressByteSize() const override { unimplemented; }
+  uint32_t GetDependentModules(lldb_private::FileSpecList &file_list) override {
+    unimplemented;
+  }
+  bool IsExecutable() const override { unimplemented; }
+  bool GetArchitecture(lldb_private::ArchSpec &arch) override { unimplemented; }
+  void
+  CreateSections(lldb_private::SectionList &unified_section_list) override {
+    unimplemented;
+  }
+  lldb_private::Symtab *GetSymtab() override { unimplemented; }
+  bool IsStripped() override { unimplemented; }
+  bool GetUUID(lldb_private::UUID *uuid) override { unimplemented; }
+  lldb::ByteOrder GetByteOrder() const override { unimplemented; }
+  bool ParseHeader() override { unimplemented; }
+  Type CalculateType() override { unimplemented; }
+  Strata CalculateStrata() override { unimplemented; }
+  lldb_private::ConstString GetPluginName() override { unimplemented; }
+  uint32_t GetPluginVersion() override { unimplemented; }
+#undef unimplemented
+};
+
+class ObjectFileDummy : public ObjectFileUnimplemented {
+public:
+  ObjectFileDummy(const lldb::ModuleSP &Module,
+                  const lldb::DataBufferSP &Buffer)
+      : ObjectFileUnimplemented(Module, Buffer) {}
+};
+
+class LLDBInitializer {
+public:
+  LLDBInitializer();
+  ~LLDBInitializer();
+};
 
 class LLDBHelper {
 public:
   LLDBHelper();
-  ~LLDBHelper();
 
   void load(const char *DLL, const char *PDB);
   static std::string mangleName(llvm::pdb::PDBSymbolFunc &Func);
   lldb_private::SymbolFile *getSymbolFile() { return SymbolFile.get(); }
 
 private:
+  LLDBInitializer LLDBInit; // This has to be first.
   lldb::DebuggerSP Debugger;
   lldb::ModuleSP Module;
   lldb::DataBufferSP Buffer;
