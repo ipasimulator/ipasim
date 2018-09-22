3ae41f3b5f8230f104b031342aa4f708b8f54f1c
Jan Joneš <jjones@outlook.cz>
2018-09-22 14:07:28 +0200

    [cg] Added a note.

--- src/HeadersAnalyzer/LLDBHelper.cpp
+++ src/HeadersAnalyzer/LLDBHelper.cpp
@@ -72,7 +72,8 @@ bool TypeComparer::areEquivalent(llvm::FunctionType *Func,
                                  const SymbolTy &SymbolFunc) {
   auto *Func2 = static_cast<llvm::FunctionType *>(getLLVMType(SymbolFunc));
   if (!Func2) {
-    // TODO: Compare signatures from header files, then.
+    // TODO: Compare signatures from header files, then. Maybe by loading those
+    // headers into LLDB.
     reportError(
         llvm::Twine(
             "cannot compare signatures of a function and non-typed symbol `") +
