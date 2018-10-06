aa304b89947a3ce68aae8726135a5a613000d121
Jan Joneš <jjones@outlook.cz>
2018-10-06 19:01:51 +0200

    [build] Installed Ninja.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,6 +1,6 @@
 ## See [docker-script].
 
 # Run CMake
-mkdir -Force cmake
+mkdir -Force cmake >$null
 cd cmake
 cmake -G Ninja ..
--- scripts/prepare-image.ps1
+++ scripts/prepare-image.ps1
@@ -7,12 +7,6 @@ Set-ExecutionPolicy Bypass -Scope Process -Force
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco feature disable --name showDownloadProgress
 
-# Make `refreshenv` available right away, by defining the
-# $env:ChocolateyInstall variable and importing the Chocolatey profile module.
-# See https://stackoverflow.com/a/46760714
-$env:ChocolateyInstall = Convert-Path "$((Get-Command choco).path)\..\.."
-Import-Module "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-
 # Install build tools.
 choco install cmake --version 3.12.2 -y --installargs 'ADD_CMAKE_TO_PATH=User'
-refreshenv
+choco install ninja --version 1.7.2 -y
