af3de7a6eff47f2f671858d1526872d09f30f50e
Jan Joneš <jjones@outlook.cz>
2018-10-07 12:41:28 +0200

    [build] Removed `rc.cmd` and made tests not to use it.

--- Dockerfile
+++ Dockerfile
@@ -19,12 +19,4 @@ RUN powershell -c " \
 # Install LLVM + Clang.
 RUN powershell -c "choco install llvm --version 7.0.0 -y"
 
-# HACK: Ideally, we would like to use `-DCMAKE_RC_COMPILER=llvm-rc`, but it
-# currently doesn't work - see
-# <https://gitlab.kitware.com/cmake/cmake/issues/17804>. So, at least for now,
-# we add a wrapper script named `rc` that wraps `llvm-rc`.
-COPY ["scripts/rc.cmd", "C:/Program Files/LLVM/bin/rc.cmd"]
-RUN powershell -c "Import-Module \"$env:ChocolateyInstall/helpers/chocolateyInstaller.psm1\"; \
-    Install-BinFile -Name rc -Path \"C:/Program Files/LLVM/bin/rc.cmd\""
-
 CMD powershell -f scripts/build.ps1
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,7 +1,12 @@
 ## See [docker-script].
 
-# Run CMake.
+# Run CMake. We use LLVM + Clang, no Visual Studio is installed. Hence, we need
+# `CMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY`, so that CMake doesn't use
+# linking when testing C and C++ compiler. And that's good, because the test
+# linking requires also resource compiler and other tools we do not have on our
+# build platform (because we don't need them).
 mkdir -Force cmake >$null
 pushd cmake
-cmake -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl -DCMAKE_LINKER=lld-link -DCMAKE_RC_COMPILER=llvm-rc ..
+cmake -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl `
+    -DCMAKE_LINKER=lld-link -DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY ..
 popd
