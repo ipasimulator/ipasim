ad5e2f1feb7e7ad443157f454a3a1a18b8218436
Jan Joneš <jjones@outlook.cz>
2018-10-20 12:26:53 +0200

    [build] Used `lld-link`.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,11 +1,8 @@
 ## See [docker-script].
 
-# Run CMake. We use LLVM + Clang, no Visual Studio is installed. Hence, we need
-# `CMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY`, so that CMake doesn't use
-# linking when testing C and C++ compiler. And that's good, because the test
-# linking requires also resource compiler and other tools we do not have on our
-# build platform (because we don't need them).
+# Run CMake.
 mkdir -Force cmake >$null
 pushd cmake
-cmake -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl ..
+cmake -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl `
+    -DCMAKE_LINKER=lld-link ..
 popd
