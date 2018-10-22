1aa7b9fad4c9ab208549fa094f0df8af5fad8183
Jan Joneš <jjones@outlook.cz>
2018-10-22 17:41:29 +0200

    [build] Used superbuild.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,6 +3,7 @@
 # Run CMake.
 mkdir -Force cmake >$null
 pushd cmake
+# TODO: This is not up-to-date. Something like `cmake -GNinja ..` works.
 cmake -GNinja -DCMAKE_C_COMPILER=clang-cl -DCMAKE_CXX_COMPILER=clang-cl `
     -DCMAKE_LINKER=lld-link ..
 popd
