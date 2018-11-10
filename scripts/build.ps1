a88ad860b5eb716391190f2fb69a280bf71ed34f
Jan Joneš <jjones@outlook.cz>
2018-11-10 22:18:06 +0100

    Revert "[build] Add volume `C:/build`"
    This reverts commit cd609017c98f3fe0ecd7b32184db4e6f00fce732.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -3,5 +3,6 @@
 # Run CMake. See #3.
 # TODO: When we use CMake v3.13, rewrite this to
 # `cmake -G Ninja -S <source_dir> -B <build_dir>`.
+mkdir -Force "C:/build" >$null
 pushd "C:/build"
 cmake -G Ninja "C:/project"
