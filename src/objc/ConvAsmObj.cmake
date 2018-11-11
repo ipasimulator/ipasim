fad68aa121bc25f714e1d01cf0783cba499879cf
Jan Joneš <jjones@outlook.cz>
2018-11-11 15:49:40 +0100

    [objc] Convert object files only when necessary
    First, `.s` files are compiled using `add_library (... OBJECT ...)` into
    object files. Then, those object files are converted with script
    `ConvAsmObj.cmake` into files `1.obj` to `N.obj` where N is number of
    `.s` files. Since this number is known at configure time, we can easily
    enumerate these files and list them as outputs of our custom command and
    as dependencies of library `objc`. Thanks to that, the conversion is
    invoked only when inputs change - unlike previously where the conversion
    (and also the linking afterwards) happened always.

--- src/objc/ConvAsmObj.cmake
+++ src/objc/ConvAsmObj.cmake
@@ -1,6 +1,7 @@
+set (I 1)
 foreach (asm_obj ${ASM_OBJECTS})
     execute_process (
         COMMAND "${SOURCE_DIR}/deps/objconv/objconv.exe"
-            -fcoff -v0 "${asm_obj}" "${asm_obj}-tmp")
-    file (RENAME "${asm_obj}-tmp" "${asm_obj}")
+            -fcoff -v0 "${asm_obj}" "${TARGET_DIR}/${I}.obj")
+    math (EXPR I "${I}+1")
 endforeach (asm_obj)
