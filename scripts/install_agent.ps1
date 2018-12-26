c655ba784e2888fdc6bf30a45b67dd82a96a34f6
Jan Joneš <jjones@outlook.cz>
2018-12-26 18:45:42 +0100

    [build] Install agent into `C:\`

--- scripts/install_agent.ps1
+++ scripts/install_agent.ps1
@@ -1,10 +1,11 @@
 # This script is meant to be run inside Azure VM to configure build agent. Don't
 # forget to provide `$env:AZURE_PAT`.
 
+cd \
 wget https://vstsagentpackage.azureedge.net/agent/2.144.0/vsts-agent-win-x64-2.144.0.zip -OutFile agent.zip
-mkdir agent
+mkdir a
 Add-Type -AssemblyName System.IO.Compression.FileSystem
-[System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\agent.zip", "$PWD\agent")
+[System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\agent.zip", "$PWD\a")
 rm agent.zip
-cd agent
+cd a
 .\config.cmd --unattended --url https://jjones.visualstudio.com --auth pat --token $env:AZURE_PAT --runAsService --runAsAutoLogon --pool Default --agent AzureVmAgent
