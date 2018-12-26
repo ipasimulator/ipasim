1a6d8b24c420a73b6ca1a457ff404a467c638ef2
Jan Joneš <jjones@outlook.cz>
2018-12-26 20:14:17 +0100

    [build] Make script `install_agent` idempotent

--- scripts/install_agent.ps1
+++ scripts/install_agent.ps1
@@ -3,9 +3,10 @@
 
 cd \
 wget https://vstsagentpackage.azureedge.net/agent/2.144.0/vsts-agent-win-x64-2.144.0.zip -OutFile agent.zip
+rm -r a
 mkdir a
 Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\agent.zip", "$PWD\a")
 rm agent.zip
 cd a
-.\config.cmd --unattended --url https://jjones.visualstudio.com --auth pat --token $env:AZURE_PAT --runAsService --runAsAutoLogon --pool Default --agent AzureVmAgent
+.\config.cmd --unattended --url https://jjones.visualstudio.com --auth pat --token $env:AZURE_PAT --runAsService --runAsAutoLogon --pool Default --agent AzureVmAgent --replace
