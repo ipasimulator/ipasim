718b22496286d22186f73529e69a789277dd7085
Jan Joneš <jjones@outlook.cz>
2018-12-28 11:20:25 +0100

    [build] Don't add Clang to sources
    It's not really necessary and doesn't actually make any sense.

--- scripts/CommonVariables.cmake
+++ scripts/CommonVariables.cmake
@@ -42,13 +42,8 @@ function (add_prep_dep target)
     if (NOT USE_ORIG_CLANG)
         add_dependencies ("${target}" prep)
         get_target_property (srcs "${target}" SOURCES)
-        target_sources ("${target}" PUBLIC
-            "${BUILT_CLANG_EXE}"
-            "${BUILT_LLD_LINK_EXE}")
-        set_source_files_properties ("${BUILT_CLANG_EXE}" "${BUILT_LLD_LINK_EXE}"
-            PROPERTIES HEADER_FILE_ONLY ON GENERATED ON)
-        set_source_files_properties (${srcs}
-            PROPERTIES OBJECT_DEPENDS "${BUILT_CLANG_EXE};${BUILT_LLD_LINK_EXE}")
+        set_source_files_properties (${srcs} PROPERTIES
+            OBJECT_DEPENDS "${BUILT_CLANG_EXE};${BUILT_LLD_LINK_EXE}")
     endif (NOT USE_ORIG_CLANG)
 endfunction (add_prep_dep)
 
