9c139eb8320bd1c5a1a95a538772ec1025e82868
Jan Joneš <jjones@outlook.cz>
2018-12-29 15:55:41 +0100

    [build] Add instructions on configuring Azure VM

--- scripts/install_agent.ps1
+++ scripts/install_agent.ps1
@@ -16,6 +16,5 @@ cd a
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 
-# Start Docker. Inspired by
-# <https://forums.docker.com/t/restart-docker-service-from-command-line/27331/2>.
-Start-Service docker
+# TODO: Make `VSTSAgent` service depend on `docker` (by `sc config VSTSAgent... depend=docker`).
+# Then, manually (or maybe automate this, too) change Docker and VSTS Agent services to run Automatically, for Local System account and to restart on failures.
