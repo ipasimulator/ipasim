65832352254d67259e5135b6ae4569ede70dc2c0
Jan Joneš <jjones@outlook.cz>
2019-02-12 00:24:01 +0100

    [uwp] Load `IpaSimLibrary` in `IpaSimApp`

--- src/IpaSimulator/IpaSimulator.cpp
+++ src/IpaSimulator/IpaSimulator.cpp
@@ -8,7 +8,9 @@ __declspec(dllexport) int main() {
     bool const rs4 = ApiInformation::IsApiContractPresent(
         L"Windows.Foundation.UniversalApiContract", 6);
     printf("Am I running on Redstone 4? %s\n", rs4 ? "Yes!" : "No. :(");
+    return rs4;
   } catch (const hresult_error &ex) {
     printf("Error: %ls\n", ex.message().c_str());
+    return ex.to_abi();
   }
 }
