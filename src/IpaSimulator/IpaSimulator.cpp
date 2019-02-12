cffdf590c5b4b436811d3ad516f4875ea49a6c1a
Jan Joneš <jjones@outlook.cz>
2019-02-12 15:02:24 +0100

    [uwp] Name nouns with first letter capital

--- src/IpaSimulator/IpaSimulator.cpp
+++ src/IpaSimulator/IpaSimulator.cpp
@@ -33,121 +33,122 @@ template <class T> inline T &operator^=(T &a, T b) {
 
 class DynamicLoader {
 public:
-  DynamicLoader(uc_engine *uc) : uc(uc) {}
-  void load(const string &path) {
-    unique_ptr<FatBinary> fat(Parser::parse(path));
+  DynamicLoader(uc_engine *UC) : UC(UC) {}
+  void load(const string &Path) {
+    unique_ptr<FatBinary> Fat(Parser::parse(Path));
 
     // TODO: Select the correct binary more intelligently.
-    Binary &bin = fat->at(0);
+    Binary &Bin = Fat->at(0);
 
     // Check header.
-    Header &hdr = bin.header();
-    if (hdr.cpu_type() != CPU_TYPES::CPU_TYPE_ARM)
+    Header &Hdr = Bin.header();
+    if (Hdr.cpu_type() != CPU_TYPES::CPU_TYPE_ARM)
       error("expected ARM binary");
     // Ensure that segments are continuous (required by `relocateSegment`).
-    if (hdr.has(HEADER_FLAGS::MH_SPLIT_SEGS))
+    if (Hdr.has(HEADER_FLAGS::MH_SPLIT_SEGS))
       error("MH_SPLIT_SEGS not supported");
-    if (!canSegmentsSlide(bin))
+    if (!canSegmentsSlide(Bin))
       error("the binary is not slideable");
 
     // Compute total size of all segments. Note that in Mach-O, segments must
     // slide together (see `ImageLoaderMachO::segmentsMustSlideTogether`).
     // Inspired by `ImageLoaderMachO::assignSegmentAddresses`.
-    uint64_t lowAddr = (uint64_t)(-1);
-    uint64_t highAddr = 0;
-    for (SegmentCommand &seg : bin.segments()) {
-      uint64_t segLow = seg.virtual_address();
+    uint64_t LowAddr = (uint64_t)(-1);
+    uint64_t HighAddr = 0;
+    for (SegmentCommand &Seg : Bin.segments()) {
+      uint64_t SegLow = Seg.virtual_address();
       // Round to page size (as required by unicorn and what even dyld does).
-      uint64_t segHigh =
-          ((segLow + seg.virtual_size()) + pageSize - 1) & (-pageSize);
-      if (segLow < highAddr) {
+      uint64_t SegHigh =
+          ((SegLow + Seg.virtual_size()) + PageSize - 1) & (-PageSize);
+      if ((SegLow < HighAddr && SegLow >= LowAddr) ||
+          (SegHigh > LowAddr && SegHigh <= HighAddr)) {
         error("overlapping segments (after rounding to pagesize)");
       }
-      if (segLow < lowAddr) {
-        lowAddr = segLow;
+      if (SegLow < LowAddr) {
+        LowAddr = SegLow;
       }
-      if (segHigh > highAddr) {
-        highAddr = segHigh;
+      if (SegHigh > HighAddr) {
+        HighAddr = SegHigh;
       }
     }
 
     // Allocate space for the segments.
-    uintptr_t addr = (uintptr_t)_aligned_malloc(highAddr - lowAddr, pageSize);
-    if (!addr)
+    uintptr_t Addr = (uintptr_t)_aligned_malloc(HighAddr - LowAddr, PageSize);
+    if (!Addr)
       error("couldn't allocate memory for segments");
-    uint64_t slide = addr - lowAddr;
+    uint64_t Slide = Addr - LowAddr;
 
     // Load segments. Inspired by `ImageLoaderMachO::mapSegments`.
-    for (SegmentCommand &seg : bin.segments()) {
+    for (SegmentCommand &Seg : Bin.segments()) {
       // Convert protection.
-      uint32_t vmprot = seg.init_protection();
-      uc_prot perms = UC_PROT_NONE;
-      if (vmprot & (uint32_t)VM_PROTECTIONS::VM_PROT_READ) {
-        perms |= UC_PROT_READ;
+      uint32_t VMProt = Seg.init_protection();
+      uc_prot Perms = UC_PROT_NONE;
+      if (VMProt & (uint32_t)VM_PROTECTIONS::VM_PROT_READ) {
+        Perms |= UC_PROT_READ;
       }
-      if (vmprot & (uint32_t)VM_PROTECTIONS::VM_PROT_WRITE) {
-        perms |= UC_PROT_WRITE;
+      if (VMProt & (uint32_t)VM_PROTECTIONS::VM_PROT_WRITE) {
+        Perms |= UC_PROT_WRITE;
       }
-      if (vmprot & (uint32_t)VM_PROTECTIONS::VM_PROT_EXECUTE) {
-        perms |= UC_PROT_EXEC;
+      if (VMProt & (uint32_t)VM_PROTECTIONS::VM_PROT_EXECUTE) {
+        Perms |= UC_PROT_EXEC;
       }
 
-      uint64_t vaddr = unsigned(seg.virtual_address()) + slide;
+      uint64_t VAddr = unsigned(Seg.virtual_address()) + Slide;
       // Emulated virtual address is actually equal to the "real" virtual
       // address.
-      uint8_t *mem = (uint8_t *)vaddr;
-      uint64_t vsize = seg.virtual_size();
+      uint8_t *Mem = (uint8_t *)VAddr;
+      uint64_t VSize = Seg.virtual_size();
 
-      if (perms == UC_PROT_NONE) {
+      if (Perms == UC_PROT_NONE) {
         // No protection means we don't have to copy any data, we just map it.
-        mapMemory(vaddr, vsize, perms, mem);
+        mapMemory(VAddr, VSize, Perms, Mem);
       } else {
         // TODO: Memory-map the segment instead of copying it.
-        auto &buff = seg.content();
+        auto &Buff = Seg.content();
         // TODO: Copy to the end of the allocated space if flag `SG_HIGHVM` is
         // present.
-        memcpy(mem, buff.data(), buff.size());
-        mapMemory(vaddr, vsize, perms, mem);
+        memcpy(Mem, Buff.data(), Buff.size());
+        mapMemory(VAddr, VSize, Perms, Mem);
 
         // Clear the remaining memory.
-        if (buff.size() < vsize)
-          memset(mem + buff.size(), 0, vsize - buff.size());
+        if (Buff.size() < VSize)
+          memset(Mem + Buff.size(), 0, VSize - Buff.size());
       }
     }
   }
 
 private:
   // Reports non-fatal error to the user.
-  void error(const string &msg) {
-    MessageDialog dlg(to_hstring("Error occured: " + msg));
-    dlg.ShowAsync();
+  void error(const string &Msg) {
+    MessageDialog Dlg(to_hstring("Error occured: " + Msg));
+    Dlg.ShowAsync();
   }
   // Inspired by `ImageLoaderMachO::segmentsCanSlide`.
-  bool canSegmentsSlide(Binary &bin) {
-    auto ftype = bin.header().file_type();
-    return ftype == FILE_TYPES::MH_DYLIB || ftype == FILE_TYPES::MH_BUNDLE ||
-           (ftype == FILE_TYPES::MH_EXECUTE && bin.is_pie());
+  bool canSegmentsSlide(Binary &Bin) {
+    auto FType = Bin.header().file_type();
+    return FType == FILE_TYPES::MH_DYLIB || FType == FILE_TYPES::MH_BUNDLE ||
+           (FType == FILE_TYPES::MH_EXECUTE && Bin.is_pie());
   }
-  void mapMemory(uint64_t addr, uint64_t size, uc_prot perms, uint8_t *mem) {
-    if (uc_mem_map_ptr(uc, addr, size, perms, mem))
+  void mapMemory(uint64_t Addr, uint64_t Size, uc_prot Perms, uint8_t *Mem) {
+    if (uc_mem_map_ptr(UC, Addr, Size, Perms, Mem))
       error("error while mapping memory into Unicorn Engine");
   }
 
-  static constexpr int pageSize = 4096;
-  uc_engine *const uc;
+  static constexpr int PageSize = 4096;
+  uc_engine *const UC;
 };
 
 extern "C" __declspec(dllexport) void start() {
   // Initialize Unicorn Engine.
-  uc_engine *uc;
-  uc_open(UC_ARCH_ARM, UC_MODE_ARM, &uc); // TODO: Handle errors.
+  uc_engine *UC;
+  uc_open(UC_ARCH_ARM, UC_MODE_ARM, &UC); // TODO: Handle errors.
 
   // Load test binary `ToDo`.
-  filesystem::path dir(Package::Current().InstalledLocation().Path().c_str());
-  DynamicLoader dyld(uc);
-  dyld.load((dir / "ToDo").string());
+  filesystem::path Dir(Package::Current().InstalledLocation().Path().c_str());
+  DynamicLoader Dyld(UC);
+  Dyld.load((Dir / "ToDo").string());
 
   // Let the user know we're done. This is here for testing purposes only.
-  MessageDialog dlg(L"Done.");
-  dlg.ShowAsync();
+  MessageDialog Dlg(L"Done.");
+  Dlg.ShowAsync();
 }
