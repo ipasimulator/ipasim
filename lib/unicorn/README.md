a333485afee81659a59995385cd008e9050340bc
Jan Joneš <jjones@outlook.cz>
2019-02-12 01:04:27 +0100

    [uwp] Add pre-built Unicorn Engine

---
+++ lib/unicorn/README.md
@@ -0,0 +1,5 @@
+# Unicorn Engine
+
+This folder contains prebuilt Unicorn Engine. Binaries are for 32-bit Windows.
+Currently, it's version 1.0.1 downloaded from
+[official Unicorn download page](http://www.unicorn-engine.org/download/).
--- src/IpaSimulator/IpaSimulator.cpp
+++ src/IpaSimulator/IpaSimulator.cpp
@@ -1,9 +1,14 @@
+#include <unicorn/unicorn.h>
 #include <winrt/Windows.Foundation.Metadata.h>
 
 using namespace winrt;
 using namespace Windows::Foundation::Metadata;
 
 __declspec(dllexport) int main() {
+  // Initialize Unicorn Engine.
+  uc_engine *uc;
+  uc_open(UC_ARCH_ARM, UC_MODE_ARM, &uc); // TODO: Handle errors.
+
   try {
     bool const rs4 = ApiInformation::IsApiContractPresent(
         L"Windows.Foundation.UniversalApiContract", 6);
