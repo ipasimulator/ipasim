6987f4af014182ce3175f3e8216f986df0d2e754
Jan Joneš <jjones@outlook.cz>
2019-02-16 11:52:35 +0100

    [build] Use `robocopy`

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -1,4 +1,3 @@
 ## See #3.
 
-if (Test-Path C:/ipaSim/src/cmake) { rm -r C:/ipaSim/src/cmake }
-cp -r -Container C:/ipaSim/build C:/ipaSim/src/cmake
+robocopy C:/ipaSim/build C:/ipaSim/src/cmake * /MIR /NP /NFL /NDL
--- scripts/restore.ps1
+++ scripts/restore.ps1
@@ -1,4 +1,3 @@
 ## See #3.
 
-if (Test-Path C:/ipaSim/build) { rm -r C:/ipaSim/build }
-cp -r -Container C:/ipaSim/src/cmake C:/ipaSim/build
+robocopy C:/ipaSim/src/cmake C:/ipaSim/build * /MIR /NP /NFL /NDL
