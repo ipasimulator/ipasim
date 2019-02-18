bb73cd457b6e3b54a4471b9de3330236405d5284
Jan Joneš <jjones@outlook.cz>
2019-02-18 11:53:57 +0100

    [woc] Initialize Dyld in `__CFInitialize`

--- src/dyld/dyld_initializer.cpp
+++ src/dyld/dyld_initializer.cpp
@@ -1,14 +1,10 @@
-// Compiled with:
-// ..\..\build\bin\clang -target i386-pc-windows-msvc -c dyld_initializer.cpp -o ..\..\Debug\dyld_initializer.obj -g -gcodeview
-
 extern "C" char _mh_dylib_header;
-extern "C" void _dyld_initialize(void*);
+extern "C" void _dyld_initialize(void *);
 extern "C" void _objc_init(void);
 
-__attribute__((constructor(1)))
-static void initializer() {
-    // Register itself within `dyld`.
-    _dyld_initialize(&_mh_dylib_header);
-    // Initialize the runtime if not already initialized.
-    _objc_init();
+extern "C" __attribute__((constructor(1))) void ipasim_initializer() {
+  // Register itself within `dyld`.
+  _dyld_initialize(&_mh_dylib_header);
+  // Initialize the runtime if not already initialized.
+  _objc_init();
 }
