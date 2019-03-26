f32aa4010a950670fb1db50471bc5e0527cbc9cb
Jan Joneš <jjones@outlook.cz>
2019-03-26 17:03:30 +0100

    [cg] Describe an issue (#28)

---
+++ samples/issue28.mm
@@ -0,0 +1,35 @@
+// See #28.
+
+struct BigStr {
+    int One;
+    int Two;
+    int Three;
+    int Four;
+    int Five;
+    int Six;
+};
+
+struct SmallStr {
+    int One;
+    int Two;
+};
+
+@interface MyCls
++ (BigStr)getMeBigStr;
++ (SmallStr)getMeSmallStr;
+@end
+
+@implementation MyCls
++ (BigStr)getMeBigStr {
+    return BigStr{1,2,3,4,5,6};
+}
++ (SmallStr)getMeSmallStr {
+    return SmallStr{1,2};
+}
+@end
+
+int main() {
+    BigStr BS([MyCls getMeBigStr]);
+    SmallStr SS([MyCls getMeSmallStr]);
+    return BS.Six + SS.Two;
+}
