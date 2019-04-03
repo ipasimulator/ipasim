7b2bb892d62abfce6d63ddbca88eafd043f710ec
Jan Joneš <jjones@outlook.cz>
2019-04-03 15:39:05 +0200

    [uwp] Add class `StdLogger`

--- include/ipasim/Logger.hpp
+++ include/ipasim/Logger.hpp
@@ -3,6 +3,8 @@
 #ifndef IPASIM_LOGGER_HPP
 #define IPASIM_LOGGER_HPP
 
+#include <iostream>
+#include <ostream>
 #include <string>
 #include <winrt/base.h>
 
@@ -12,46 +14,81 @@ class EndToken {};
 class WinErrorToken {};
 class AppendWinErrorToken {};
 
-class DebugStream {
+template <typename DerivedTy> class Stream {
 public:
-  using Handler = std::function<void(DebugStream &)>;
+  using Handler = std::function<void(DerivedTy &)>;
 
-  DebugStream &operator<<(const std::string &S) { return *this << S.c_str(); }
-  DebugStream &operator<<(const char *S) {
-    OutputDebugStringA(S);
-    return *this;
-  }
-  DebugStream &operator<<(const std::wstring &S) { return *this << S.c_str(); }
-  DebugStream &operator<<(const wchar_t *S) {
-    OutputDebugStringW(S);
-    return *this;
-  }
-  DebugStream &operator<<(EndToken) { return *this << ".\n"; }
-  DebugStream &operator<<(WinErrorToken) {
+  // Note that all `operator<<`s must be here and none inside `DerivedTy`,
+  // because otherwise only those in `DerivedTy` would be searched.
+  DerivedTy &operator<<(const char *S) { return d().write(S); }
+  DerivedTy &operator<<(const wchar_t *S) { return d().write(S); }
+  DerivedTy &operator<<(const std::string &S) { return d() << S.c_str(); }
+  DerivedTy &operator<<(const std::wstring &S) { return d() << S.c_str(); }
+  DerivedTy &operator<<(EndToken) { return d() << ".\n"; }
+  DerivedTy &operator<<(WinErrorToken) {
     using namespace winrt;
 
     // From `winrt::throw_last_error`
     hresult_error Err(HRESULT_FROM_WIN32(GetLastError()));
-    return *this << Err.message().c_str();
+    return d() << Err.message().c_str();
   }
-  DebugStream &operator<<(AppendWinErrorToken) {
-    return *this << EndToken() << WinErrorToken() << "\n";
+  DerivedTy &operator<<(AppendWinErrorToken) {
+    return d() << EndToken() << WinErrorToken() << "\n";
   }
   template <typename T>
   std::enable_if_t<
       std::is_same_v<decltype(std::to_string(std::declval<T>())), std::string>,
-      DebugStream &>
+      DerivedTy &>
   operator<<(const T &Any) {
-    return *this << std::to_string(Any).c_str();
+    return d() << std::to_string(Any).c_str();
   }
-  DebugStream &operator<<(const Handler &Handler) {
-    Handler(*this);
+  DerivedTy &operator<<(const Handler &Handler) {
+    Handler(d());
+    return d();
+  }
+
+private:
+  DerivedTy &d() { return *static_cast<DerivedTy *>(this); }
+};
+
+class DebugStream : public Stream<DebugStream> {
+public:
+  DebugStream &write(const char *S) {
+    OutputDebugStringA(S);
+    return *this;
+  }
+  DebugStream &write(const wchar_t *S) {
+    OutputDebugStringW(S);
     return *this;
   }
 };
 
+class StdStream : public Stream<StdStream> {
+public:
+  StdStream(std::ostream &Str, std::wostream &WStr) : Str(Str), WStr(WStr) {}
+
+  StdStream &write(const char *S) {
+    Str << S;
+    return *this;
+  }
+  StdStream &write(const wchar_t *S) {
+    WStr << S;
+    return *this;
+  }
+
+  static StdStream out() { return StdStream(std::cout, std::wcout); }
+  static StdStream err() { return StdStream(std::cerr, std::wcerr); }
+
+private:
+  std::ostream &Str;
+  std::wostream &WStr;
+};
+
 template <typename StreamTy> class Logger {
 public:
+  Logger() = default;
+  Logger(StreamTy &&O, StreamTy &&E) : O(std::move(O)), E(std::move(E)) {}
+
   void error(const std::string &Message) {
     error</* AppendLastError */ false>(Message);
   }
@@ -78,6 +115,7 @@ private:
 };
 
 using DebugLogger = Logger<DebugStream>;
+using StdLogger = Logger<StdStream>;
 
 } // namespace ipasim
 
