ba80e089a270a615b3990a439e43dd2007863d89
Jan Joneš <jjones@outlook.cz>
2019-04-09 22:10:14 +0200

    [build] Select subscription

--- scripts/ci/auth.ps1
+++ scripts/ci/auth.ps1
@@ -7,3 +7,4 @@ $key = ConvertTo-SecureString $env:AZURE_KEY -AsPlainText -Force
 $cred = New-Object `
     System.Management.Automation.PSCredential($env:AZURE_APP_ID, $key)
 Add-AzureAccount -Credential $cred -Tenant $env:AZURE_TENANT
+Select-AzureSubscription -SubscriptionId 363eb770-e7e5-49a1-b1eb-e9df07ee11ac
