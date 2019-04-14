9653b96d05f919e90bffb3f6122bac58a8a58dcf
Jan Joneš <jjones@outlook.cz>
2019-04-14 11:10:45 +0200

    [objc] Be more verbose about `.obj` conversion

--- scripts/ConvAsmObj.cmake
+++ scripts/ConvAsmObj.cmake
@@ -2,6 +2,6 @@ set (I 1)
 foreach (asm_obj ${ASM_OBJECTS})
     execute_process (
         COMMAND "${SOURCE_DIR}/deps/objconv/objconv.exe"
-            -fcoff -v0 "${asm_obj}" "${TARGET_DIR}/${I}.obj")
+            -fcoff -v1 "${asm_obj}" "${TARGET_DIR}/${I}.obj")
     math (EXPR I "${I}+1")
 endforeach (asm_obj)
