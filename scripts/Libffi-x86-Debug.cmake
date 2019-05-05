73097166a78684d0a747757239347cc48be883a8
Jan Joneš <jjones@outlook.cz>
2019-05-06 00:04:53 +0200

    [build] Configure `libffi` for 32 bits

--- scripts/Libffi-x86-Debug.cmake
+++ scripts/Libffi-x86-Debug.cmake
@@ -6,5 +6,6 @@ execute_process (
     COMMAND "${CMAKE_COMMAND}" -G Ninja
         -DCMAKE_BUILD_TYPE=Debug
         -DCMAKE_EXPORT_COMPILE_COMMANDS=On
+        -DHAVE_64BIT=Off
         "${SOURCE_DIR}/deps/Libffi"
     WORKING_DIRECTORY "${DEBUG_LIBFFI_CMAKE_DIR}")
--- scripts/Libffi-x86-Release.cmake
+++ scripts/Libffi-x86-Release.cmake
@@ -6,5 +6,6 @@ execute_process (
     COMMAND "${CMAKE_COMMAND}" -G Ninja
         -DCMAKE_BUILD_TYPE=Release
         -DCMAKE_EXPORT_COMPILE_COMMANDS=On
+        -DHAVE_64BIT=Off
         "${SOURCE_DIR}/deps/Libffi"
     WORKING_DIRECTORY "${RELEASE_LIBFFI_CMAKE_DIR}")
