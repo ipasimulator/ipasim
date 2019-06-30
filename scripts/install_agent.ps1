3efc35cdb769078839ef47b2ac9ba2ce772788df
Jan Joneš <jjones@outlook.cz>
2019-06-30 16:23:38 +0200

    [build] Update agent

--- scripts/install_agent.ps1
+++ scripts/install_agent.ps1
@@ -12,7 +12,7 @@ Add-Type -AssemblyName System.IO.Compression.FileSystem
 [System.IO.Compression.ZipFile]::ExtractToDirectory("$PWD\agent.zip", "$PWD\a")
 rm agent.zip
 cd a
-.\config.cmd --unattended --url https://jjones.visualstudio.com --auth pat --token $args[0] --runAsService --runAsAutoLogon --pool Default --agent AzureVmAgent --replace
+.\config.cmd --unattended --url https://dev.azure.com/ipasim --auth pat --token $args[0] --runAsService --runAsAutoLogon --pool Default --agent AzureVmAgent --replace
 
 # Install `docker-compose`. Inspired by
 # <https://docs.docker.com/compose/install/#install-compose>.
@@ -20,7 +20,7 @@ cd a
 Invoke-WebRequest "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-Windows-x86_64.exe" -UseBasicParsing -OutFile $Env:ProgramFiles\docker\docker-compose.exe
 
 # Make `VSTSAgent` service depends on `docker`.
-cmd /c sc config vstsagent.jjones.AzureVmAgent depend=docker
+cmd /c sc config vstsagent.ipasim.AzureVmAgent depend=docker
 
 # TODO: Change Docker and VSTS Agent services to run Automatically, for Local System account and to restart on failures.
 # TODO: Copy file `./scripts/daemon.json` into `%programdata%/docker/config/daemon.json`.
