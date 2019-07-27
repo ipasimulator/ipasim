c92ddbca61b8a21dd7d0fabb906086617ffe78c7
Jan Joneš <jjones@outlook.cz>
2019-07-27 08:37:35 +0200

    [build] Build Libffi outside the source tree (i30)

--- scripts/extract.ps1
+++ scripts/extract.ps1
@@ -8,6 +8,7 @@ if (($Configuration -cne "Release") -and ($Configuration -cne "Debug")) {
 robocopy C:/ipaSim/build/ipasim-x86-$Configuration/bin C:/ipaSim/src/cmake/ipasim-x86-$Configuration/bin * /MIR /NP /NFL /NDL
 robocopy C:/ipaSim/build/winobjc-x86-$Configuration/bin C:/ipaSim/src/cmake/winobjc-x86-$Configuration/bin * /MIR /NP /NFL /NDL
 robocopy C:/ipaSim/build/lief-x86-$Configuration/include C:/ipaSim/src/cmake/lief-x86-$Configuration/include * /MIR /NP /NFL /NDL
+robocopy C:/ipaSim/build/Libffi-x86-$Configuration/build/include C:/ipaSim/src/cmake/Libffi-x86-$Configuration/build/include * /MIR /NP /NFL /NDL
 robocopy C:/ipaSim/build/ipasim-x86-$Configuration/gen C:/ipaSim/src/src/IpaSimulator/IpaSimApp/gen * /MIR /NP /NFL /NDL
 robocopy C:/ipaSim/src/deps/WinObjC/Frameworks/UIKit.Xaml/prebuilt/$Configuration C:/ipaSim/src/src/IpaSimulator/IpaSimApp/UIKit.Xaml *.xbf /MIR /NP /NFL /NDL
 robocopy C:/ipaSim/src/deps/WinObjC/Frameworks/Social.Xaml/prebuilt/$Configuration C:/ipaSim/src/src/IpaSimulator/IpaSimApp/Social.Xaml *.xbf /MIR /NP /NFL /NDL
